6c44270a9dc9e66bdb51d8765ab7d3f89a3c268e
Matija Pretnar <matija@pretnar.info>
2026-04-20 13:33:51 +0200

    Primer s predavanj

--- 07-funkcijsko-programiranje/predavanja/src/main.rs
+++ 07-funkcijsko-programiranje/predavanja/src/main.rs
@@ -1,62 +1,111 @@
-fn primer1() {
-    let v1 = vec![2, 3, 5, 7, 11, 13];
-    let f = |x| x + 1;
-    let v2 : Vec<i32> = v1.iter().map(f).collect();
-    println!("{:?}", v2)
+fn skalarni_produkt_zanka(vec1 : Vec<f64>, vec2 : Vec<f64>) -> f64 {
+    let mut vsota = 0.;
+    let mut i = 0;
+    while i < vec1.len() {
+        i += 1;
+        vsota += vec1[i] * vec2[i]
+    };
+    vsota
 }
 
-// struct Podatki_za_f_iz_primera2 {
-//     a: &Box<i32>
-// }
-// impl Closure for Podatki_za_f_iz_primera2 {
-//     fn call(self, x: &i32) {
-//         x + *self.a
-//     }
-// }
+fn zmnozi((x1, x2): (&f64, &f64)) -> f64 {
+    x1 * x2
+}
+
+fn skalarni_produkt_iter(vec1 : Vec<f64>, vec2 : Vec<f64>) -> f64 {
+    vec1.iter().zip(vec2.iter())
+               .map(|(x1, x2)| x1 * x2)
+               .sum()
+}
+
+fn primer1() {
+    // struct TipSamoZaFunkcijoIzPrimera1 {
+    // }
+    // impl Fn(i32) -> i32 for TipSamoZaFunkcijoIzPrimera1 {
+    //    fn call(&self, x: i32) -> i32 {
+    //        x + 1
+    //    }
+    // }
+    let f = |x| x + 1;
+    println!("{}", f(1));
+    println!("{}", f(2));
+    println!("{}", f(3));
+}
+
 
 fn primer2() {
-    let v1 = vec![2, 3, 5, 7, 11, 13];
-    let a = Box::new(10);
-    // let okolje_f = struct { a = &a }
-    let f = |x| x + *a;
-    // f = Podatki_za_f_iz_primera2 {
-    //    a = a
+    let a = String::from("test");
+    // struct TipSamoZaFunkcijoIzPrimera2 {
+    //     a: &String
     // }
-    let v2 : Vec<i32> = v1.iter().map(f).collect();
-    println!("{:?}", v2)
+    // impl Fn(String) for TipSamoZaFunkcijoIzPrimera2 {
+    //     fn call(&self, s: String) {
+    //         println!("{}{}", s, self.a)
+    //     }
+    // }
+    let f = |s| println!("{}{}", s, a);
+    f(String::from("a"));
+    f(String::from("b"));
+    f(String::from("c"));
+    println!("{a}")
 }
-
-// struct Podatki_za_f_iz_primera3 {
-//     a: &mut Vec<i32>
-// }
 
 fn primer3() {
-    let v1 = vec![2, 3, 5, 7, 11, 13];
-    let mut w: Vec<i32> = vec![];
-    let mut f = |x| { w.push(10 * x) };
-    // let okolje_f = struct { w = &mut a }
-    for x in v1 {
-        f(x);
+    let stevila  = vec![10, 20, 30];
+    let mut funkcije = vec![];
+    for x in stevila.iter() {
+        // struct TipSamoZaFunkcijoIzPrimera3 {
+        //     x: i32
+        // }
+        funkcije.push(|y| x.clone() * y);
+        // lahko tudi:
+        // funkcije.push(move |y| x * y);
     }
-    println!("{:?}", w)
-}
-
-struct Podatki_za_f_iz_primera4 {
-    a: Box<i32>
+    for f in funkcije.iter() {
+        println!("{}", f(7))
+    }
 }
 
 fn primer4() {
-    let v1 = vec![2];
-    let a = Box::new(10);
-    let f = |x| { let b = a; x + *b };
-    // ne dela, ker bi f poklicali več kot enkrat
-    // let v2 : Vec<i32> = v1.iter().map(f).collect();
-    let b = f(32);
-    // let c = f(32);
-    println!("{:?}", b)
+    let mut a = String::from("test");
+    // struct TipSamoZaFunkcijoIzPrimera4 {
+    //     a: &mut String
+    // }
+    // impl FnMut(String) for TipSamoZaFunkcijoIzPrimera4 {
+    //     fn call(&mut self, s: String) {
+    //         self.a.push_str("t");
+    //         println!("{}{}", s, self.a)
+    //     }
+    // }
+    let mut f = |s| {
+        a.push_str("t");
+        println!("{}{}", s, a)
+    };
+    f(String::from("a"));
+    f(String::from("b"));
+    f(String::from("c"));
+    println!("{a}")
+}
+
+fn primer5() {
+    let a = String::from("test");
+    // struct TipSamoZaFunkcijoIzPrimera5 {
+    //     a: String
+    // }
+    let f = |s| {
+        let b = a;
+        println!("{}{}", s, b)
+    };
+    f(String::from("a"));
+    // spodnje ne delajo, ker je f prevzel lastništvo nad a, zato ga ne more več uporabiti
+    // f(String::from("b"));
+    // f(String::from("c"));
+    // println!("{a}")
 }
 
 fn main() {
-    let x = 10;
-    let v = [(|a| {a + x}), (|a| {a + x})];
+    let mut s = String::from("Hello");
+    let mut add_suffix = || s.push_str(" world");
+    println!("{s}");
+    add_suffix();
 }
