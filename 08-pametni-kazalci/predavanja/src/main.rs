9ae76f09d8d3e21edd3753af323d757c5141f796
Matija Pretnar <matija@pretnar.info>
2026-05-05 11:15:26 +0200

    Gradiva s predavanj

--- 08-pametni-kazalci/predavanja/src/main.rs
+++ 08-pametni-kazalci/predavanja/src/main.rs
@@ -1,67 +1,84 @@
-use std::{
-    ops::{Deref, DerefMut},
-    rc::Rc,
-};
+use std::ops::{Deref, DerefMut};
+use std::rc::Rc;
 
-struct MemoryBox {
-    contents: i32,
-    history: Vec<i32>,
+struct MyBox<T> {
+    value: T,
+    history: Vec<T>,
 }
 
-impl MemoryBox {
-    fn new(i: i32) -> Self {
-        MemoryBox {
-            contents: i,
+impl<T> MyBox<T> {
+    fn new(value: T) -> Self {
+        MyBox {
+            value,
             history: vec![],
         }
     }
 }
 
-impl Deref for MemoryBox {
-    type Target = i32;
+impl<T> Deref for MyBox<T> {
+    type Target = T;
 
-    fn deref(&self) -> &i32 {
-        &self.contents
+    fn deref(&self) -> &T {
+        &self.value
     }
 }
 
-impl DerefMut for MemoryBox {
-    fn deref_mut(&mut self) -> &mut Self::Target {
-        self.history.push(self.contents);
-        &mut self.contents
+impl<T: Clone> DerefMut for MyBox<T> {
+    fn deref_mut(&mut self) -> &mut T {
+        self.history.push(self.value.clone());
+        &mut self.value
     }
 }
 
-impl Drop for MemoryBox {
+impl<T> Drop for MyBox<T> {
     fn drop(&mut self) {
-        println!("All important moments flashing before my eyes...");
-        for i in self.history.iter() {
-            println!("{i}, aaahh....")
-        }
+        println!(
+            "Preden izginem, naj povem, da sem se spremenil {}-krat",
+            self.history.len()
+        );
     }
 }
 
-#[derive(Clone, Debug)]
-enum List {
-    Cons(i32, Rc<List>),
+#[derive(Clone)]
+enum BoxList<T> {
     Nil,
+    Cons (T, Box<BoxList<T>>)
 }
 
-use crate::List::{Cons, Nil};
+enum RcList<T> {
+    Nil,
+    Cons (T, Rc<RcList<T>>)
+}
+
 
 fn main() {
-    let mut n = MemoryBox::new(42);
-    for i in 1..10 {
-        *n = *n + i;
-        println!("{}", *n);
-    }
+    let x = Box::new(5);
+    println!("Vrednost v škatli je {}", *x);
+    let mut y = MyBox::new(5);
+    println!("Vrednost v moji škatli je {}", *y);
+    *y = 10;
+    println!("Vrednost v moji škatli je {}", *y);
+    *y = 20;
+    println!("Vrednost v moji škatli je {}", *y);
 
-    let mut xss = Vec::new();
-    let mut xs = Rc::new(Nil);
-    // pri daljših seznamih nastopi težava s prekoračitvijo sklada
-    // zaradi rekurzivnega drop-a
-    for x in 1..20000 {
-        xss.push(Rc::clone(&xs));
-        xs = Rc::new(Cons(x, xs));
+    let mut xs : Box<BoxList<i32>> = Box::new(BoxList::Nil);
+    let mut xss : BoxList<BoxList<i32>> = BoxList::Nil;
+    let start = std::time::Instant::now();
+    for i in 1..400 {
+        let ys = xs.clone();
+        xs = Box::new(BoxList::Cons(i, xs));
+        xss = BoxList::Cons(*ys, Box::new(xss));
     }
+    println!("Čas: {:?}", start.elapsed());
+
+
+    let mut xs : Rc<RcList<i32>> = Rc::new(RcList::Nil);
+    let mut xss : RcList<Rc<RcList<i32>>> = RcList::Nil;
+    let start = std::time::Instant::now();
+    for i in 1..8000 {
+        let ys = Rc::clone(&xs);
+        xs = Rc::new(RcList::Cons(i, xs));
+        xss = RcList::Cons(ys, Rc::new(xss));
+    }
+    println!("Čas: {:?}", start.elapsed());
 }
