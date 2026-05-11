c86b6072f98b59af251936e809e72879c7ea318c
Matija Pretnar <matija@pretnar.info>
2026-05-11 09:08:06 +0200

    Primeri s predavanj

--- 09-vecnitno-izvajanje/predavanja/src/main.rs
+++ 09-vecnitno-izvajanje/predavanja/src/main.rs
@@ -1,58 +1,73 @@
 use rand::Rng;
-use std::collections::HashMap;
-use std::{thread, time};
 use std::sync::mpsc;
+use std::{thread, time};
 
 fn print_pro_tip(i: i32) {
-    // let delay = rand::thread_rng().gen_range(1000..=5000);
+    let delay = rand::thread_rng().gen_range(1000..=5000);
     thread::sleep(time::Duration::from_millis(delay));
     let n = 42 - i;
-    println!("Pro tip #{i}: {n} + {i} = 42");
+    println!("Pro tip #{i}: {n} + {i} = 42 ({delay}ms)");
 }
 
-fn send_pro_tip(i: i32, tx: mpsc::Sender<(i32, String)>) {
-    // let delay = rand::thread_rng().gen_range(1000..=5000);
-    // thread::sleep(time::Duration::from_millis(delay));
+fn pocasno_zaporedno_izvajanje() {
+    for i in 1..5000 {
+        print_pro_tip(i);
+    }
+}
+
+fn vzporedne_niti_ki_jih_prehitro_opustimo() {
+    for i in 1..5000 {
+        thread::spawn(move || print_pro_tip(i));
+    }
+    println!("Malo bom še počakal");
+    thread::sleep(time::Duration::from_millis(500));
+    println!("Pa še malo");
+    thread::sleep(time::Duration::from_millis(100));
+    println!("Zdaj pa bom končal");
+}
+
+fn vzporedne_niti_ki_jih_sproti_cakamo() {
+    for i in 1..5000 {
+        let h = thread::spawn(move || print_pro_tip(i));
+        h.join();
+    }
+}
+
+fn vzporedne_niti_ki_ustrezno_pocakamo() {
+    let mut hs = vec![];
+    for i in 1..5000 {
+        let h = thread::spawn(move || print_pro_tip(i));
+        hs.push(h);
+    }
+    for h in hs {
+        h.join();
+    }
+}
+
+fn send_pro_tip(i: i32, tx: mpsc::Sender<String>) {
+    let delay = rand::thread_rng().gen_range(1000..=5000);
+    thread::sleep(time::Duration::from_millis(delay));
     let n = 42 - i;
-    tx.send((i, format!("Pro tip #{i}: {n} + {i} = 42")));
+    tx.send(format!("Pro tip #{i}: {n} + {i} = 42 ({delay}ms)"));
 }
 
-fn read_pro_tips(rx: mpsc::Receiver<(i32, String)>) {
-    let mut pro_tips = HashMap::new();
-    let mut i = 1;
-    for (n, pro_tip) in rx {
-        pro_tips.insert(n, pro_tip);
-        while let Some(pro_tip) = pro_tips.get(&i) {
-            i = i + 1;
-            println!("{pro_tip}");
-        }
+fn vzporedne_niti_ki_komunicirajo_s_klicateljem() {
+    let mut hs = vec![];
+    let (tx, rx) = mpsc::channel();
+    for i in 1..5000 {
+        let tx_clone = tx.clone();
+        let h = thread::spawn(move || send_pro_tip(i, tx_clone));
+        hs.push(h);
+    }
+    for m in rx {
+        println!("{m}");
     }
 }
 
 fn main() {
-    for i in 1..5000 {
-        print_pro_tip(i);
-    }
-
-    // let mut handles = vec![];
-    // for i in 1..10000 {
-    //     println!("{i}");
-    //     let handle = thread::spawn(move || print_pro_tip(i));
-    //     handles.push(handle);
-    // }
-    // for handle in handles {
-    //     handle.join();
-    // }
-
-    // let mut handles = vec![];
-    // let (tx, rx) = mpsc::channel();
-    // thread::spawn(|| read_pro_tips(rx));
-    // for i in 1..5000 {
-    //     let tx_clone = tx.clone();
-    //     let handle = thread::spawn(move || send_pro_tip(i, tx_clone));
-    //     handles.push(handle);
-    // }
-    // for handle in handles {
-    //     handle.join();
-    // }
+    pocasno_zaporedno_izvajanje();
+    vzporedne_niti_ki_jih_prehitro_opustimo();
+    vzporedne_niti_ki_jih_sproti_cakamo();
+    vzporedne_niti_ki_ustrezno_pocakamo();
+    vzporedne_niti_ki_komunicirajo_s_klicateljem();
 }
